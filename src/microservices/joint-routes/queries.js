23e4cc44d93d5dc68b1959887e01bfe1336cd598
Guiselle Tatiana Zambrano Penagos <gtzambranop@unal.edu.co>
2021-05-04 02:44:02 -0500

    errors :'c

--- src/microservices/joint-routes/queries.js
+++ src/microservices/joint-routes/queries.js
@@ -5,10 +5,19 @@ const { HttpProvider } = require('../../providers');
 const baseUrl = process.env.MS_JOINT_ROUTES_BASE_URL;
 
 module.exports = {
-  getAllJoinRoutes: async () => {
-    return await HttpProvider.get(`${baseUrl}/routes`);
+  // Coordinate
+  getCoordinateById: async (root, {data}) => {
+    return await HttpProvider.get(`${baseUrl}/coordinate`, data);
   },
-  getJoinRouteById: async (root, { id }) => {
-    return await HttpProvider.get(`${baseUrl}/routes/${id}`);
+  // User
+  getUserById: async (root, {data}) => {
+    return await HttpProvider.get(`${baseUrl}/user`, data);
+  },
+  // Route
+  getRouteById: async (root, { routeId }) => {
+    return await HttpProvider.get(`${baseUrl}/route/${routeId}`);
+  },
+  getPublicRoutes: async () => {
+    return await HttpProvider.get(`${baseUrl}/route/public`);
   },
 };
